116e423603193cd4d96412f51a405f598dfe88ae
Olej <oleg.andreyev@do-it.co>
2017-03-14 01:03:06 +0200

    cors

--- server/app.js
+++ server/app.js
@@ -39,7 +39,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', routes);
 
